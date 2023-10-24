827dcc65d3635b925fec0a394b8a1aae14ae65ed
Chaitanya Sharma <chait8126@gmail.com>
2023-10-24 12:46:52 +0530

    async

--- AsyncAwaitTask/Program.cs
+++ AsyncAwaitTask/Program.cs
@@ -2,9 +2,27 @@
 {
 	public class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
-			Console.WriteLine(MakeTea());
+			Console.WriteLine(await MakeTeaAsync());
+		}
+		static async Task<string> MakeTeaAsync()
+		{
+			var boilingTask = BoilWaterAsync();
+			Console.WriteLine("take the cups out");
+			Console.WriteLine("put tea in cup");
+			var water = await boilingTask;
+			var tea = $"pour {water} into the cup";
+			Console.WriteLine(tea);
+			return "serve tea";
+		}
+		static async Task<string> BoilWaterAsync()
+		{
+			Console.WriteLine("Start the kettle");
+			Console.WriteLine("Waiting for the kettle");
+			await Task.Delay(20000);
+			Console.WriteLine("Kettle finished boiling");
+			return "Boiled Water";
 		}
 		static string MakeTea()
 		{
