fdd476cc4d000b2189d28817710815faca933925
Chaitanya Sharma <chait8126@gmail.com>
2023-10-24 12:26:42 +0530

    not async

--- AsyncAwaitTask/Program.cs
+++ AsyncAwaitTask/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace AsyncAwaitTask
 {
-	internal class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Console.WriteLine(MakeTea());
 		}
+		static string MakeTea()
+		{
+			var water = BoilWater();
+            Console.WriteLine("take the cups out");
+            Console.WriteLine("put tea in cup");
+            var tea = $"pour {water} into the cup";
+			Console.WriteLine(tea);
+			return tea;
+
+        }
+		static string BoilWater()
+		{
+            Console.WriteLine("Start the kettle"); 
+			Console.WriteLine("Waiting for the kettle");
+            Task.Delay(20000).GetAwaiter().GetResult();//a synchronous delay
+            //.getResult blocks the current thread untill the task is completed
+            Console.WriteLine("Kettle finished boiling");
+			return "Boiled Water";
+        }
 	}
 }
